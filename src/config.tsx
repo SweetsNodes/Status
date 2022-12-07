673a46829f39412d60640a07ca80526a047483fb
Jirayu <114326534+SweetsNodes@users.noreply.github.com>
2022-12-07 20:56:10 +0700

    Delete config.tsx

--- src/config.tsx
+++
@@ -1,8 +0,0 @@
-export const name: string = "SweetsNodes";
-export const interval: number = 60;
-export const nodes: any[] = [
-  {
-    name: "TH1",
-    url: "http://th1.sweetsnodes.online:1800",
-  },
-];
